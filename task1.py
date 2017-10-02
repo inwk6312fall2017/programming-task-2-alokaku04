9f9d08afaa1ed56a4383af99345e3c7b719f517b
alokaku04ls <alok.ranjan@dal.ca>
2017-10-02 12:56:48 -0300

    test5

--- task1.py
+++ task1.py
@@ -3,8 +3,10 @@ y=[]
 for line in f:
     q=f.readline()
     y.append(q)
+
+
 def is_strip(y):
-    x=[]
+    x = []
     for element in y:
         element.strip()
         x.append(element)
@@ -12,6 +14,8 @@ def is_strip(y):
     for element in x:
         if element==ASSAULT:
             count += 1
-        z.append(element, count)
+    print(count)
     return z
+
+
 print(is_strip(y))
